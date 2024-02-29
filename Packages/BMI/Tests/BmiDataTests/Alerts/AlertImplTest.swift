0da839eaadc4a29a18c2d15f2c74fc89cde34813
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-02-29 13:55:41 +0000

    Tests for BmiData complete and passing

---
+++ Packages/BMI/Tests/BmiDataTests/Alerts/AlertImplTest.swift
@@ -0,0 +1,34 @@
+//
+//  GenerateAlertImplTest.swift
+//  
+//
+//  Created by Tharin Zaman on 29/02/2024.
+//
+
+import XCTest
+
+final class GenerateAlertImplTest: XCTestCase {
+    
+    private let 
+    
+    func test_invalidBmiError_success() {
+        // ASSIGN
+        
+        // ACT
+        let result =
+        // ASSERT
+    }
+    
+    func test_invalidWeightError_success() {
+        
+    }
+    
+    func test_invalidHeightError_success() {
+        
+    }
+    
+    func test_otherError_success() {
+        
+    }
+
+}
