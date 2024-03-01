d403cc3f681a0f375285d74bc6997ff5c7ca47ef
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-03-01 13:50:29 +0000

    ExerciseListScreen and button created

---
+++ Packages/Networking/Sources/NetworkingDomain/Entities/NetworkingEntity.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//
+//
+//  Created by Tharin Zaman on 26/02/2024.
+//
+
+import Foundation
+
+public struct NetworkingEntity: Decodable, Equatable {
+    
+    public let exercise: String
+    public let description: String
+    public let bodyPart: String
+    
+//    public var id: UUID {
+//        return UUID()
+//    }
+    
+}
