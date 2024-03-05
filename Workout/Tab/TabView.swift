a6af6b9827e75867fab5c8eae2f8d54ceff96261
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-03-05 19:02:06 +0000

    WorkoutTabViewStarted

---
+++ Workout/Tab/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Workout
+//
+//  Created by Tharin Zaman on 05/03/2024.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
